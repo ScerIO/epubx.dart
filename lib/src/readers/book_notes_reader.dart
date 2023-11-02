a7b7f5da06384f463fd0b1b755cc520e84e3d549
Maksym Loia <knemo7362@gmail.com>
2023-11-02 17:42:53 +0200

    t

--- lib/src/readers/book_notes_reader.dart
+++ lib/src/readers/book_notes_reader.dart
@@ -1,36 +1,20 @@
 import 'dart:async';
 
 import 'package:collection/collection.dart' show IterableExtension;
-
-import '../ref_entities/epub_book_ref.dart';
-import '../schema/opf/epub_manifest_item.dart';
-import '../schema/opf/epub_metadata_meta.dart';
+import 'package:epubx/epubx.dart';
 
 class BookNotesReader {
   static Future<dynamic> readBookNotes(EpubBookRef bookRef) async {
-    var metaItems = bookRef.Schema!.Package!.Metadata!.MetaItems;
-    if (metaItems == null || metaItems.isEmpty) return null;
-
-    var coverMetaItem = metaItems.firstWhereOrNull(
-        (EpubMetadataMeta metaItem) =>
-            metaItem.Name != null &&
-            metaItem.Name!.toLowerCase().contains('note'));
-    if (coverMetaItem == null) return null;
-    if (coverMetaItem.Content == null || coverMetaItem.Content!.isEmpty) {
-      throw Exception(
-          'Incorrect EPUB metadata: cover item content is missing.');
-    }
-
-    var coverManifestItem = bookRef.Schema!.Package!.Manifest!.Items!
+    var noteManifestItem = bookRef.Schema!.Package!.Manifest!.Items!
         .firstWhereOrNull((EpubManifestItem manifestItem) =>
-            manifestItem.Id!.toLowerCase() ==
-            coverMetaItem.Content!.toLowerCase());
-    if (coverManifestItem == null) {
+            manifestItem.Id!.toLowerCase().contains('note'));
+    if (noteManifestItem == null) {
       throw Exception(
-          'Incorrect EPUB manifest: item with ID = \"${coverMetaItem.Content}\" is missing.');
+          'Incorrect EPUB manifest: item with ID = \"$noteManifestItem\" is missing.');
     }
 
-    final note = bookRef.Content?.AllFiles?[coverManifestItem.Href];
+    final note = bookRef.Content?.AllFiles?[noteManifestItem.Href];
+    final string = await note?.readContentAsText();
 
     return note;
   }
