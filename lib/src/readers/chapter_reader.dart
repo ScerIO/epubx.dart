d7b11906c7f02996d551b957e2a10e5d45f937eb
Jasmeet Singh <jasmeet0817@gmail.com>
2025-02-08 21:09:57 +0100

    Fix spline parsing and navPoint matching

--- lib/src/readers/chapter_reader.dart
+++ lib/src/readers/chapter_reader.dart
@@ -3,6 +3,8 @@ import 'package:epubx/src/utils/file_name_decoder.dart';
 
 import '../ref_entities/epub_text_content_file_ref.dart';
 
+import 'package:tuple/tuple.dart';
+
 class ChapterReader {
   static List<EpubChapterRef> getChapters(EpubBookRef bookRef) {
     if (bookRef.Schema!.Navigation == null) {
@@ -21,8 +23,12 @@ class ChapterReader {
     final spineItems = package.Spine!.Items!;
     final manifestItems = package.Manifest!.Items!;
 
-    EpubChapterRef? lastTopLevelChapter;
-    EpubNavigationPoint? lastNavPoint;
+    // This map is used to keep track of the chapters that are found in TOC.
+    // These chapters can have spline items as children and we need to add them to the correct parent.
+    final Map<EpubNavigationPoint, EpubChapterRef> navPointMap = {};
+
+    // Last chapter is used to add OtherFragments whenever a chapter is not found in TOC.
+    EpubChapterRef? lastChapter;
     for (var i = 0; i < spineItems.length; i++) {
       if (spineItems[i].IsLinear != null && !spineItems[i].IsLinear!) {
         continue;
@@ -36,60 +42,69 @@ class ChapterReader {
         continue;
       }
       final navPoint = navigationPoints.cast<EpubNavigationPoint?>().firstWhere(
-            (element) =>
-                element!.Content!.Source!.split('/').last.toLowerCase() ==
-                manifestItem.Href!.split('/').last.toLowerCase(),
+            (element) => checkEqual(element!, manifestItem),
             orElse: () => null,
           );
       if (navPoint != null) {
-        lastTopLevelChapter = navigationPointToChapter(
+        // Found in TOC. Add chapter.
+        lastChapter = navigationPointToChapter(
           bookRef,
           navPoint,
         );
-        if (lastTopLevelChapter != null) {
-          lastNavPoint = navPoint;
-          result.add(lastTopLevelChapter);
+        if (lastChapter != null) {
+          result.add(lastChapter);
+          navPointMap[navPoint] = lastChapter;
         }
         continue;
       }
-      if (lastNavPoint != null) {
-        var found = false;
-        for (var childNavPoint in lastNavPoint.ChildNavigationPoints!) {
-          if (childNavPoint.Content!.Source!.split('/').last.toLowerCase() ==
-              manifestItem.Href!.split('/').last.toLowerCase()) {
+      // Try and find in a child of a TOC item.
+      var found = false;
+      MapEntry<EpubNavigationPoint, EpubChapterRef>? foundEntry;
+      for (var navPoint in navPointMap.keys) {
+        for (var childNavPoint in navPoint.ChildNavigationPoints!) {
+          if (checkEqual(childNavPoint, manifestItem)) {
             final subChapter = navigationPointToChapter(
               bookRef,
               childNavPoint,
             );
             if (subChapter != null) {
-              lastTopLevelChapter!.SubChapters.add(subChapter);
+              // Found in a child of a TOC item. Add chapter.
+              final chapter = navPointMap[navPoint]!;
+              chapter.SubChapters.add(subChapter);
               found = true;
+              foundEntry = MapEntry(childNavPoint, subChapter);
+              lastChapter = subChapter;
               break;
             }
           }
         }
         if (found) {
-          continue;
+          break;
         }
       }
+      if (found) {
+        navPointMap.addEntries([foundEntry!]);
+        continue;
+      }
+      // Not found in TOC. Add chapter as OtherFragment.
       final point = EpubNavigationPoint();
       point.Content = EpubNavigationContent();
       point.Content!.Source = manifestItem.Href;
       point.Id = manifestItem.Id;
       point.NavigationLabels = <EpubNavigationLabel>[];
       point.NavigationLabels!.add(EpubNavigationLabel()
-        ..Text = lastTopLevelChapter != null ? 'Untitled Chapter' : 'Begining');
+        ..Text = lastChapter != null ? 'Untitled Chapter' : 'Begining');
       point.ChildNavigationPoints = <EpubNavigationPoint>[];
       final chapterFragment = navigationPointToChapter(
         bookRef,
         point,
       );
-      if (lastTopLevelChapter != null && chapterFragment != null) {
-        lastTopLevelChapter.OtherChapterFragments.add(
+      if (lastChapter != null && chapterFragment != null) {
+        lastChapter.OtherChapterFragments.add(
             chapterFragment.epubTextContentFileRef);
       } else if (chapterFragment != null) {
         result.add(chapterFragment);
-        lastTopLevelChapter = chapterFragment;
+        lastChapter = chapterFragment;
       }
     }
     return result;
@@ -99,23 +114,15 @@ class ChapterReader {
     EpubBookRef bookRef,
     EpubNavigationPoint navigationPoint,
   ) {
-    String? contentFileName;
+    String contentFileName;
     String? anchor;
     if (navigationPoint.Content?.Source == null) {
       return null;
     }
-    var contentSourceAnchorCharIndex =
-        navigationPoint.Content!.Source!.indexOf('#');
-    if (contentSourceAnchorCharIndex == -1) {
-      contentFileName = navigationPoint.Content!.Source;
-      anchor = null;
-    } else {
-      contentFileName = navigationPoint.Content!.Source!
-          .substring(0, contentSourceAnchorCharIndex);
-      anchor = navigationPoint.Content!.Source!
-          .substring(contentSourceAnchorCharIndex + 1);
-    }
-    contentFileName = decodeFileName(contentFileName!);
+    Tuple2<String, String?> splitResult =
+        splitNavPointSource(navigationPoint.Content!.Source!);
+    contentFileName = decodeFileName(splitResult.item1);
+    anchor = splitResult.item2;
     EpubTextContentFileRef? htmlContentFileRef;
     if (!bookRef.Content!.Html!.containsKey(contentFileName)) {
       throw Exception(
@@ -131,5 +138,27 @@ class ChapterReader {
     chapterRef.Anchor = anchor;
     chapterRef.Title = navigationPoint.NavigationLabels!.first.Text;
     return chapterRef;
+  }
+
+  static Tuple2<String, String?> splitNavPointSource(String source) {
+    var contentSourceAnchorCharIndex = source.indexOf('#');
+    if (contentSourceAnchorCharIndex == -1) {
+      return Tuple2(source, null);
+    } else {
+      return Tuple2(source.substring(0, contentSourceAnchorCharIndex),
+          source.substring(contentSourceAnchorCharIndex + 1));
+    }
+  }
+
+  static bool checkEqual(EpubNavigationPoint point, EpubManifestItem item) {
+    return getContentFileSource(point.Content!.Source!)
+            .split('/')
+            .last
+            .toLowerCase() ==
+        item.Href!.split('/').last.toLowerCase();
+  }
+
+  static String getContentFileSource(String source) {
+    return splitNavPointSource(source).item1;
   }
 }
